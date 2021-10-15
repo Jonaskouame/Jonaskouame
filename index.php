1047208686f54459273b845b73a71d3734b763aa
Jonaskouame <91888180+Jonaskouame@users.noreply.github.com>
2021-10-15 18:42:05 +0000

    Update index.php

--- index.php
+++ index.php
@@ -8,13 +8,14 @@ $text        = $_POST["text"];
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
     $response  = "CON What would you want to check \n";
-    $response .= "1. My Account \n";
-    $response .= "2. My phone number";
+    $response .= "1. FACEBOOK \n";
+    $response .= "2. WHATSAPP";
 
 } else if ($text == "1") {
     // Business logic for first level response
     $response = "CON Choose account information you want to view \n";
-    $response .= "1. Account number \n";
+    $response .= "1. Alerte Fake-news \n";
+    $response .= "2. Vérifier une Information \n";
 
 } else if ($text == "2") {
     // Business logic for first level response
@@ -23,7 +24,7 @@ if ($text == "") {
 
 } else if($text == "1*1") { 
     // This is a second level response where the user selected 1 in the first instance
-    $accountNumber  = "ACC1001";
+    $accountNumber  = "Cette information est un Fake-news";
 
     // This is a terminal request. Note how we start the response with END
     $response = "END Your account number is ".$accountNumber;
